8cbdf6f93e9dbbd47ba03df88078522207560a64
Dominik Finkbeiner <finkes93@gmail.com>
2014-05-06 09:46:54 +0200

    fixes scanner

---
+++ input/test.x
@@ -0,0 +1,7 @@
+program test ;
+	read x : int ;
+	print y : float ;
+	begin
+		if 3 > 4 then x := 2 ;
+		y := x * 4.5 ;
+end .
--- src/dhbw/compiler/x/Main.java
+++ src/dhbw/compiler/x/Main.java
@@ -2,10 +2,14 @@ package dhbw.compiler.x;
 
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
 import dhbw.compiler.x.tokens.Token;
+import dhbw.compiler.x.tokens.Token.TokenType;
 
 public class Main {
 
@@ -13,23 +17,39 @@ public class Main {
 	{
 //		 InputStream stream = new ByteArrayInputStream("[1,1...a,1.0^223,12,12,1as1,12.12..12.12,null,..,NULL,12..12.....]".getBytes());
 
-		 InputStream stream = new ByteArrayInputStream("[123.314]".getBytes());
-
-	        Scanner scanner = new Scanner(stream);
+		// InputStream stream = new ByteArrayInputStream("[123.314]".getBytes());
+		 
+		 FileInputStream stream;
+		try 
+		{
+			stream = new FileInputStream(new File("input/test.x"));
+			
+			Scanner scanner = new Scanner(stream);
 
 	        ArrayList<Token> tokens = new ArrayList<Token>();
 
 	        Token token;
 
-	        try {
-	            while ((token = scanner.nextToken()) != null)
-	            {
-	                System.out.print(token.toString()+" ");
-	                tokens.add(token);
-	            }
-	        }
-	        catch (Exception e) {
+            while ((token = scanner.nextToken()) != null)
+            {
+                System.out.println(token.toString()+" ");
+                tokens.add(token);
+                
+                if(token.getType() == TokenType.EOF)
+                	break;
+            }
+		        
+		} 
+		catch (FileNotFoundException e1) 
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		 catch (Exception e) {
 	            e.printStackTrace();
 	        }
+
+	      
+	       
 	}
 }
